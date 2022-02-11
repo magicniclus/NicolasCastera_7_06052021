a423b3eb6371412dcb8cde418f686a169543314d
Nicolas <casteranicolas.contact@gmail.com>
2022-02-11 18:35:34 +0100

    Modification du datamanger

--- Js/Data/dataManagers.js
+++ Js/Data/dataManagers.js
@@ -180,7 +180,7 @@ export function getRecipes() {
  *
  * @param   {string}  value  valeur saisi dans la search bar
  *
- * @return  {Array}         change le contenu du tableau searchValue 
+ * @return  {void}         change le contenu du tableau searchValue 
  */
  export function getRecipesByTagBar(value) {
     value = value.toLowerCase();
